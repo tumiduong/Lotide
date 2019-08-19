e431102d92eaa1f21fb626e40d720135b413b61e
tumiduong <duong.julie@live.com>
2019-08-19 22:10:01 +0000

    Corrected lint

--- head.js
+++ head.js
@@ -7,12 +7,9 @@ const assertEqual = function(actual, expected) {
 };
 
 const head = function(array) {
-  if (array != []) {
+  if (array !== []) {
     return array[0];
   } else {
     return undefined;
   }
 };
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
