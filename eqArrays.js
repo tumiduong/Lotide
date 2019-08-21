4751c4cdf8cde9b28212652c9aa4163977065a0f
tumiduong <duong.julie@live.com>
2019-08-21 22:28:23 +0000

    Wrote function to compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Asseration Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let arrayEq = "";
+  for (i = 0; i < array1.length; i++) {
+    for (x = 0; x < array2.length; x++) {
+      if (array1[i] === array2[x]) {
+        arrayEq = true;
+      } else {
+        arrayEq = false;
+      }
+    }
+  } return arrayEq;
+};
