91aaba88410c9e5821b75233670c5f36ea6af72d
tumiduong <duong.julie@live.com>
2019-08-21 18:15:07 +0000

    Fixed brackets

--- countOnly.js
+++ countOnly.js
@@ -16,24 +16,5 @@ const countOnly = function(allItems, itemsToCount) {
         results[item] = 1;
       }
     }
-  }
-  return results;
-}
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
+  } return results;
+};
