8725b123f4f296f679297ebdd81b683ca407773c
tumiduong <duong.julie@live.com>
2019-08-26 19:07:49 +0000

    refactored headTest.js with chai with @@JCyan90 @TheoMLP

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,24 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const head = require("../head");
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]), undefined);
+describe("#head", () => {
+  it("return 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("return '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("return 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+  it("returns 1 for [1]", () => {
+    assert.strictEqual(head([1]), 1);
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
+
