07010b45cc134a6160d9d310562e8076d54ac214
tumiduong <duong.julie@live.com>
2019-08-26 19:33:25 +0000

    refactored middleTest.js with @JCyan90 @TheoMLP

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,11 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const { assert } = require("chai");
 const middle = require("../middle");
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+describe("#middle", () => {
+  it("should return [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("should return [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+}); 
