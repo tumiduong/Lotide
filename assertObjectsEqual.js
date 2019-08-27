f65f40476326aec9f507640b03b1e73779297841
tumiduong <duong.julie@live.com>
2019-08-27 23:05:33 +0000

    assertion for equal objects

---
+++ assertObjectsEqual.js
@@ -0,0 +1,25 @@
+const eqObjects = require('./eqObjects');
+const inspect = require('util').inspect;
+
+const assertObjectsEqual = function(object1, object2) {
+  result = eqObjects(object1, object2);
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(object1)} === ${inspect(object2)}`);
+  } else {
+    console.log(`🛑🛑🛑 Asseration Failed: ${inspect(object1)} !== ${inspect(object2)}`);
+  }
+};
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual(ab, ba);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(ab, abc);
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(cd, dc);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(cd, cd2);
+
+module.exports = assertObjectsEqual;
