2aee3090a2a3253c37bbdf527fed9b3bbad68b7d
tumiduong <duong.julie@live.com>
2019-08-27 21:45:35 +0000

    wrote letterPositions

--- letterPositions.js
+++ letterPositions.js
@@ -1,9 +1,15 @@
 const letterPositions = function(sentence) {
   const results = {};
   for (x = 0; x < sentence.length; x++) {
-    if (sentence[x] === " ") {
-
+    if (!results[sentence[x]]) {
+      results[sentence[x]] = [x];
+    } else if (Array.isArray(results[sentence[x]])) {
+      results[sentence[x]].push(x)
     }
-  }
+  } delete results[" "];
   return results;
 };
+
+console.log(letterPositions("hello there"));
+
+module.exports = letterPositions;
