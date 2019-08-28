5b960fed42be89e0a9dedd6a88341fa39afa956e
tumiduong <duong.julie@live.com>
2019-08-28 02:01:58 +0000

    find key by value in object

---
+++ findKey.js
@@ -0,0 +1,25 @@
+const assertEqual = require("./assertEqual");
+
+const findKey = function(object, callback) {
+  let keyOfObject = Object.keys(object);
+  let trueKey = [];
+  let findThisKey;
+  for (let x = 0; x < keyOfObject.length; x++) {
+    trueKey.push(callback(object[keyOfObject[x]])); 
+  }
+  for (let i = 0; i < trueKey.length; i++) { 
+    if (trueKey[i]) {
+      findThisKey = keyOfObject[i];
+      return findThisKey;
+    }
+  }
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2))
